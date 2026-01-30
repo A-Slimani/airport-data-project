c3c6a5a3b63a6c18267325230d485b0fdf8c8df6
Abdullah Slimani <abdullahslimani98@gmail.com>
2026-01-30 17:20:06 +1100

    updates

--- azure-functions/function_app.py
+++ azure-functions/function_app.py
@@ -52,15 +52,22 @@ def syd_airport_scraper(req: func.HttpRequest) -> func.HttpResponse:
 
 @app.function_name("AdelaideAirportScraper")
 @app.route(route="adelaide-scraper", methods=['GET'])
-def adl_airport_scraper(timer: func.HttpRequest) -> func.HttpResponse:
+def adl_airport_scraper(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        logging.info("Starting Adelaide..scraper")
+        logging.info("Starting Adelaide scraper")
 
         filename = f"adelaide-{DATE_YESTERDAY}.json"
         data = get_data_adl()
         upload_blob_az(data, filename, f"RAW/ADL")
 
         logging.info("Adelaide scraper complete")
+
+        return func.HttpResponse(
+            json.dumps({"status":"SUCCESS"}),
+            status_code=200,
+            mimetype="application/json"
+        )
+
     except Exception as e:
         logging.info("Adelaide scraper failed")
         return func.HttpResponse(
@@ -73,36 +80,81 @@ def adl_airport_scraper(timer: func.HttpRequest) -> func.HttpResponse:
 
 @app.function_name("BrisbaneAirportScraper")
 @app.route(route="brisbane-scraper", methods=['GET'])
-def bri_airport_scraper(timer: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Starting Brisbane scraper")
+def bri_airport_scraper(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        logging.info("Starting Brisbane scraper")
 
-    filename = f"brisbane-{DATE_YESTERDAY}.json"
-    data = get_data_bri()
-    upload_blob_az(data, filename, f"RAW/BRI")
+        filename = f"brisbane-{DATE_YESTERDAY}.json"
+        data = get_data_bri()
+        upload_blob_az(data, filename, f"RAW/BRI")
 
-    logging.info("Brisbane scraper complete")
+        logging.info("Brisbane scraper complete")
+
+        return func.HttpResponse(
+            json.dumps({"status":"SUCCESS"}),
+            status_code=200,
+            mimetype="application/json"
+        )
+
+    except Exception as e:
+        logging.info("Brisbane scraper failed")
+        return func.HttpResponse(
+            json.dumps({"status":"FAILED", "error": str(e)}),
+            status_code=500,
+            mimetype="application/json"
+        )
 
 
 @app.function_name("MelbourneAirportScraper")
 @app.route(route="melbourne-scraper", methods=['GET'])
-def mel_airport_scraper(timer: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Starting Melbourne..scraper")
+def mel_airport_scraper(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        logging.info("Starting Melbourne scraper")
 
-    filename = f"melbourne-{DATE_YESTERDAY}.json"
-    data = get_data_mel(TIMESTAMP_YESTERDAY_START, TIMESTAMP_YESTERDAY_END)
-    upload_blob_az(data, filename, f"RAW/MEL")
+        filename = f"melbourne-{DATE_YESTERDAY}.json"
+        data = get_data_mel(TIMESTAMP_YESTERDAY_START, TIMESTAMP_YESTERDAY_END)
+        upload_blob_az(data, filename, f"RAW/MEL")
 
-    logging.info("Melbourne scraper complete")
+        logging.info("Melbourne scraper complete")
+
+        return func.HttpResponse(
+            json.dumps({"status":"SUCCESS"}),
+            status_code=200,
+            mimetype="application/json"
+        )
+
+    except Exception as e:
+        logging.info("Melbourne scraper failed")
+        return func.HttpResponse(
+            json.dumps({"status":"FAILED", "error": str(e)}),
+            status_code=500,
+            mimetype="application/json"
+        )
 
 
 @app.function_name("PerthAirportScraper")
 @app.route(route="perth-scraper", methods=['GET'])
-def per_airport_scraper(timer: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Starting Perth scraper")
+def per_airport_scraper(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        logging.info("Starting Perth scraper")
 
-    filename = f"perth-{DATE_TODAY}.json"
-    data = get_data_per()
-    upload_blob_az(data, filename, f"RAW/PER")
+        filename = f"perth-{DATE_TODAY}.json"
+        data = get_data_per()
+        upload_blob_az(data, filename, f"RAW/PER")
 
-    logging.info("Perth scraper complete")
+        logging.info("Perth scraper complete")
+        
+        return func.HttpResponse(
+            json.dumps({"status":"SUCCESS"}),
+            status_code=200,
+            mimetype="application/json"
+        )
+
+    except Exception as e:
+        logging.info("Perth scraper failed")
+        return func.HttpResponse(
+            json.dumps({"status":"FAILED", "error": str(e)}),
+            status_code=500,
+            mimetype="application/json"
+        )
 
